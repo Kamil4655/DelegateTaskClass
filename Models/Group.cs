56b54c5fe12dfb34919264206f6004def628f4c6
fuadmammadov33 <fuaddybala3@gmail.com>
2024-04-19 11:45:02 +0400

    DelegateTAskClass

---
+++ Models/Group.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateTask.Models
+{
+    internal class Group
+    {
+        private int Id { get; set; }
+        private string Name { get; set; }
+
+        public Group(string name)
+        {
+            Name = name;
+            Id++;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using DelegateTask.Models;
+
+namespace DelegateTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Group ab106 = new Group("AB106"); 
+            
+            Student studentname = new Student("Kamil", "Qehremanov", 20,350); 
+            List<Student> student = new List<Student>();
+        }
+    }
+}
